6628d56458b761df524a70413265ad606871af91
Anthony Hudson <anthony@dfrgrp.com>
2026-01-11 06:38:07 -0600

    fix: use 3-part SemVer for Velopack compatibility

--- AgentConnect/MainWindow.xaml.cs
+++ AgentConnect/MainWindow.xaml.cs
@@ -2,6 +2,8 @@ using System.Windows;
 
 #if DEBUG
 using AgentConnect.DevTools;
+using AgentConnect.Updates.Models;
+using AgentConnect.Updates.UI;
 #endif
 
 namespace AgentConnect
@@ -18,7 +20,29 @@ namespace AgentConnect
 #if DEBUG
             // Add debug overlay - auto-attaches to this window's events
             RootGrid.Children.Add(new UIMetricsOverlay());
+
+            // Show debug button for testing update prompt
+            DebugUpdateButton.Visibility = Visibility.Visible;
 #endif
         }
+
+#if DEBUG
+        private void DebugUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mockUpdateInfo = new ExtendedUpdateInfo
+            {
+                TargetVersion = "1.1.0.0",
+                Type = UpdateType.Prompted,
+                ReleaseNotes = "• New feature: Auto-update system\n• Improved performance\n• Bug fixes and stability improvements",
+                MaxDeferrals = 3,
+                IsCritical = false
+            };
+
+            var mockUpdateService = new MockUpdateService();
+            var updateWindow = new UpdatePromptWindow(mockUpdateInfo, mockUpdateService);
+            updateWindow.Owner = this;
+            updateWindow.ShowDialog();
+        }
+#endif
     }
 }
